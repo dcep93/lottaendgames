03d347094711b3dfce89ec07f5615e216887310a
Daniel Cepeda <dcep93@gmail.com>
2026-07-12 15:46:00 -0400

    Clean up chapter text extraction

--- app/src/app_x/chapterPayloadManifest.ts
+++ app/src/app_x/chapterPayloadManifest.ts
@@ -1,2 +1,2 @@
-export const chapterPayloadContentHash = 'sha256:e5ace8b896ccd3735cdcd5dc9e6ebc17ff23e07965d5768f2b47f08952065a3a'
-export const chapterPayloadPath = 'app_x/chapters.e5ace8b896ccd373.json'
+export const chapterPayloadContentHash = 'sha256:e8ad3be7660ca52fc02afd770646bb1e557ca08c5c2792b1a2d2965d2723e67e'
+export const chapterPayloadPath = 'app_x/chapters.e8ad3be7660ca52f.json'
