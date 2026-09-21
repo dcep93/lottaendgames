18f204f17f835b1ed44e11fd5c48d0a0bc132301
Daniel Cepeda <dcep93@gmail.com>
2026-09-21 16:23:33 -0400

    Prefer Kc7 after Kd6 Kb5 in the supported seven setup

--- app/src/mate/rules/bishopKnightSupportedPreferences.ts
+++ app/src/mate/rules/bishopKnightSupportedPreferences.ts
@@ -2,6 +2,7 @@ import { SQUARE_TRANSFORMS, transformFen, transformSquare } from '../chess'
 
 // Exact r2.5 declarations, subordinate to r1.5; counters do not affect placement.
 const declarations = new Map(([
+  ['8/8/3K4/1k6/8/1B1N4/8/8 w - - 0 1', 'd6', 'c7'],
   ['8/5K2/3k4/8/8/1B1N4/8/8 w - - 0 1', 'f7', 'e8'],
   ['5K2/8/3k4/8/8/1B1N4/8/8 w - - 0 1', 'f8', 'e8'],
 ] as const).flatMap(([fen, from, to]) => SQUARE_TRANSFORMS.map(transform => [
